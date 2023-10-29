feebaed61732df823eb2f9889da24538073a3bbe
shehanKavindu <kavindushehansayakkara@gmail.com>
2023-10-29 14:09:02 +0530

    addController

--- BackEnd/src/main/java/lk/ijse/car/controller/AddminController.java
+++ BackEnd/src/main/java/lk/ijse/car/controller/AddminController.java
@@ -1,4 +1,15 @@
 package lk.ijse.car.controller;
 
-public class AddminController {
-}
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/admin")
+public class AdminController {
+
+    @Autowired
+    private AdminService service;
