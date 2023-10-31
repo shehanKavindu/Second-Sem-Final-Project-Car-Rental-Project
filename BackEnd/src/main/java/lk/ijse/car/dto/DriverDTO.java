baffbf610fc768adbcdd42c6b5ecc2f956f773fa
shehanKavindu <kavindushehansayakkara@gmail.com>
2023-10-31 19:24:38 +0530

    AddDriverDTO

--- BackEnd/src/main/java/lk/ijse/car/dto/DriverDTO.java
+++ BackEnd/src/main/java/lk/ijse/car/dto/DriverDTO.java
@@ -15,6 +15,15 @@ import org.springframework.web.multipart.MultipartFile;
 @AllArgsConstructor
 @ToString
 public class DriverDTO {
+    private String user_Id;
+    private Name name;
+    private String contact_No;
+    private String address;
+    private String email;
+    private String nic_No;
+    private String license_No;
+    private MultipartFile license_Img;
+    private AvailabilityType driverAvailability;
 
     private UserDTO user;
 
