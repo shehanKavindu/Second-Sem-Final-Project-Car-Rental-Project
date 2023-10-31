98256c94d6ae8f5d1d49e06fb27c2efd92e4fd82
shehanKavindu <kavindushehansayakkara@gmail.com>
2023-10-31 19:07:35 +0530

    CreatePaymentController

---
+++ BackEnd/src/main/java/lk/ijse/car/controller/PaymentController.java
@@ -0,0 +1,30 @@
+package lk.ijse.car.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@CrossOrigin
+@RequestMapping("/payment")
+public class PaymentController {
+    @Autowired
+    private PaymentService service;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @GetMapping(path = "/paymentIdGenerate")
+    public @ResponseBody CustomDTO customerIdGenerate() {
+        return service.paymentIdGenerate();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public ResponseUtil savePayment(@RequestBody PaymentDTO dto, @RequestParam String rentID) {
+        System.out.println(dto);
+        System.out.println(rentID);
+        service.savePayment(dto, rentID);
+        return new ResponseUtil("OK", "Successfully Payment.!", null);
+    }
+
+}
