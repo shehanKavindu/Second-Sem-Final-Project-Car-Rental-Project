c1ff9538d710b6487f6c9dc26d7ae44e2fbee4c9
shehanKavindu <kavindushehansayakkara@gmail.com>
2023-10-31 19:22:55 +0530

    DoneCustomerDTO

--- BackEnd/src/main/java/lk/ijse/car/dto/CustomDTO.java
+++ BackEnd/src/main/java/lk/ijse/car/dto/CustomDTO.java
@@ -13,4 +13,11 @@ public class CustomDTO {
     private String value;
     private int count;
 
+    public CustomDTO(String lastIndex) {
+        this.value=lastIndex;
+    }
+
+    public CustomDTO(int count) {
+        this.count=count;
+    }
 }
