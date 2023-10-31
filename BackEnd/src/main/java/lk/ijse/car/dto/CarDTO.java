f91faf3a6fb381b98bfd17a8578a37f233ded6b1
shehanKavindu <kavindushehansayakkara@gmail.com>
2023-10-31 19:20:40 +0530

    AddEntitys

--- BackEnd/src/main/java/lk/ijse/car/dto/CarDTO.java
+++ BackEnd/src/main/java/lk/ijse/car/dto/CarDTO.java
@@ -13,3 +13,17 @@ import lombok.ToString;
 public class CarDTO {
     private String car_Id;
 
+    private String name;
+    private String brand;
+    private CarType type;
+    private ImageDTO image;
+    private int number_Of_Passengers;
+    private TransmissionType transmission_Type;
+    private FuelType fuel_Type;
+    private Rate rent_Duration_Price;
+    private double price_Extra_KM;
+    private String registration_Number;
+    private double free_Mileage;
+    private String color;
+    private AvailabilityType vehicleAvailabilityType;
+}
