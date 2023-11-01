20b4547204af296e05ae28c0b744837ea8c49beb
shehanKavindu <kavindushehansayakkara@gmail.com>
2023-11-01 12:40:51 +0530

    Add Private keys

---
+++ BackEnd/src/main/java/lk/ijse/car/dto/Reg_UserDTO.java
@@ -0,0 +1,28 @@
+package lk.ijse.car.dto;
+
+
+import lk.ijse.car.embeded.Name;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Reg_UserDTO {
+    private String user_Id;
+    private Name name;
+    private String contact_No;
+    private String address;
+    private String email;
+    private String nic;
+    private String license_No;
+    private MultipartFile nic_Img;
+    private MultipartFile license_Img;
+
+    private UserDTO user;
+
+}
