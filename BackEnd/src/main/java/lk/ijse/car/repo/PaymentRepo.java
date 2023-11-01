e1a0929948099dfc2f28e36ed9c40b7b866c0b86
shehanKavindu <kavindushehansayakkara@gmail.com>
2023-11-01 21:19:57 +0530

    Crete Repo

---
+++ BackEnd/src/main/java/lk/ijse/car/repo/PaymentRepo.java
@@ -0,0 +1,10 @@
+package lk.ijse.car.repo;
+
+import lk.ijse.car.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface PaymentRepo extends JpaRepository<Payment, String> {
+    @Query(value = "SELECT paymentID FROM Payment ORDER BY paymentID DESC LIMIT 1", nativeQuery = true)
+    String getLastIndex();
+}
