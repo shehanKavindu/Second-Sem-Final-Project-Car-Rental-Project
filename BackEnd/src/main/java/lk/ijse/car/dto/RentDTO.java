ff4ba81738866e1709fd271db7dc477c97bebf72
shehanKavindu <kavindushehansayakkara@gmail.com>
2023-11-01 12:44:31 +0530

    Add Private Keys

---
+++ BackEnd/src/main/java/lk/ijse/car/dto/RentDTO.java
@@ -0,0 +1,33 @@
+package lk.ijse.car.dto;
+
+import com.easy.car_rental.enums.RentRequest;
+import com.easy.car_rental.enums.RequestType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Data
+public class RentDTO {
+
+    private String rentID;
+    private LocalDate pickUpDate;
+    private LocalTime pickUpTime;
+    private LocalDate returnDate;
+    private LocalTime returnTime;
+    private RequestType requestType;
+    private RentRequest rentType;
+    private String location;
+    private Reg_UserDTO regUser;
+
+    private List<RentDetailsDTO> rentDetails;
+
+}
